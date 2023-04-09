fa8b68d2bf801ac6fd35716c39e006d9488be85e
fokycnik <fokycnik.anton@mail.ru>
2023-04-09 20:47:01 +0300

    Второе задание

--- Program.cs
+++ Program.cs
@@ -22,4 +22,22 @@
 //   Console.WriteLine("Минимальное число: " + minNumber);
 // }
 
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.git 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ваше первое число: " + number1);
+// Console.WriteLine("Ваше второе число: " + number2);
+// Console.WriteLine("Ваше третье число: " + number3);
+
+// int maxnumber = number1;
+
+// if (number1 < number2) maxnumber = number2;
+// if (number1 < number3) maxnumber = number3;
+
+//   Console.WriteLine("Ваше максимальное число: " + maxnumber);
